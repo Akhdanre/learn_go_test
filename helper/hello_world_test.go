e454cb4cadc3711ddc6e485e92ea962b482051b9
Akeon <danakhdan12@gmail.com>
2024-01-21 20:30:20 +0700

    learn error test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,6 +1,9 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 
 
@@ -8,12 +11,17 @@ import "testing"
 func TestHelloWorld(t *testing.T){
 	result := HelloWorld("akhdan")
 	if result != "hello akhdan"{
-		panic("result is not 'hello akhdan'")
+		t.Error("result not same with 'hello akhdan'")
+		// t.Fail("result not same with 'hello akhdan'")
 	}
+	fmt.Println("TestHelloWorld done")
 }
 func TestHelloWorldRobbani(t *testing.T){
 	result := HelloWorld("robbani")
 	if result != "hello robbani"{
-		panic("result is not 'hello robbani'")
+		// t.FailNow("result not same with 'hello robbani'")
+		t.Fatal("result not same with 'hello robbani'")
+		
 	}
+	fmt.Println("TestHelloWorldRobbani done")
 }
